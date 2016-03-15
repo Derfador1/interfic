821c902a227dbc007c3d58142908cb8785072c93
Derfador1 <Derfador1@gmail.com>
2016-03-15 15:59:57 -0400

    last update for the day

--- area.py
+++ area.py
@@ -6,11 +6,15 @@ import time
 random.seed(time.time())
 
 class Map:
-	random_dir = ['North', 'South', 'East', 'West', 'Up', 'Down']
-	for item in random_dir:
-		random_int = random.randint(0,1)
-		if random_int == 1:
-			print(item)
-			
+	def __init__(self):
+		self._list1 = []
+	
+	def create_options(self):	
+		random_dir = ['North', 'South', 'East', 'West', 'Up', 'Down']
+		for item in random_dir:
+			random_int = random.randint(0,1)
+			if random_int == 1:
+				list1.append(item)
+	
 class Item:
 	#generate items
