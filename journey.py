3f639c5935f236846a3e131d97558ee14745ed2d
Derfador1 <Derfador1@gmail.com>
2016-03-16 15:54:25 -0400

    final commit for the end of the day

--- journey.py
+++ journey.py
@@ -15,7 +15,7 @@ def main():
 	winning_options = [
 		'get all', 'open door',
 		'east', 'get edelweiss',
-		'Up', 'enter cave', 
+		'up', 'enter cave', 
 		'light fire', 'wait', 
 		'put edelweiss in fire', 
 		'put helmet in statue', 
@@ -34,34 +34,51 @@ def main():
 	step = 0
 	
 	player = a.Player()
+	answer = []
 	
 	while True:
 		player.choose()
+
+		os.system('clear')
+
 		scene = a.Scene(winning_options[step], scene_descrip[random.randint(0, 3)])
 		scene.create_treasure(step)
 		
 		scene.check_move(player)
 		
+		response = player.get_actions()
+		
+		if response == 'That was an incorrect option':
+			print(response)
+			continue
+		
+		if step == 0 or step == 3 or step == 13:
+			if scene._action == 'move':
+				print('You should probably get the things in this room before anything else')
+				continue
+
+		print(response)
+		
 		if scene._action == 'move':
 			print('You have entered an area... ' + scene._description)
-		
+
 		if scene.get_winning_actions(player):
 			print('that was correct option')
-			#create a new list
+			answer.append(player._choice)
 			step += 1
-		else:
+		else:		#could remove this to allow player to keep going to get to good path the round about way
 			death = player.player_death()
 			print(death)
 			break
-		
-		response = player.get_actions()
-		
+				
 		if response == 'getting...' or 'taking...':
 			player._lootbag.append(scene._item1)
 			player._lootbag.append(scene._item2)
+			
+		if set(answer) == set(winning_options):
+			print('winner winner chicken dinner')
+			break
 	
-		print(response)
-
 
 if __name__ == "__main__":
 	main()
