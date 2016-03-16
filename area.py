e223fa61bab1ea9c9b9b174528bfae90d4b18a7e
Derfador1 <Derfador1@gmail.com>
2016-03-16 15:00:23 -0400

    made the items be part of the scene class

--- area.py
+++ area.py
@@ -8,15 +8,15 @@ random.seed(time.time())
 
 	
 def get():
-	output = 'Getting...'
+	output = 'getting...'
 	return output
 	
 def light():
-	output = 'Lighting...'
+	output = 'lighting...'
 	return output
 	
 def drop():
-	output = 'Dropping things...'
+	output = 'dropping things...'
 	return output
 	
 def examine():
@@ -25,6 +25,42 @@ def examine():
 	
 def open():
 	output = 'opening...'
+	return output
+
+def north():
+	output = 'going north...'
+	return output
+
+def open1():
+	output = 'opening...'
+	return output
+	
+def west():
+	output = 'going west...'
+	return output
+
+def east():
+	output = 'going east...'
+	return output
+
+def south():
+	output = 'going south...'
+	return output
+	
+def up():
+	output = 'going up...'
+	return output
+	
+def down():
+	output = 'going down...'
+	return output
+
+def enter():
+	output = 'entering...'
+	return output
+	
+def exit1():
+	output = 'exiting...'
 	return output
 
 def print_menu():
@@ -40,21 +76,31 @@ def print_menu():
 		]
 	return menu
 
-dict_choice = {'light':light, 'examine': examine, 'get':get, 'take':get, 'drop':drop}
+
+action_dict = {'light':light, 'examine': examine, 'get':get, 'take':get, 'drop':drop}
+move_dict = {'north':north, 'open':open1, 'west':west, 'east':east, 'south':south, 'up':up, 'down':down, 'enter':enter, 'exit':exit1}
 
 class Game:
 	def __init__(self, winning_action):
 		self._winning_action = winning_action
+		self._action = None
 		
 	def get_actions(self):
 		str1 = self._choice.split(' ')
 	
-		for item in dict_choice:
-			if str1[0] == item:
-				output = dict_choice[item]()
-				return(output)
+		if not str1[0] in move_dict:
+			for item in action_dict:
+				if str1[0] == item:
+					output = action_dict[item]()
+					return(output)
+		else:
+			for item in move_dict:
+				if str1[0] == item:
+					output = move_dict[item]()
+					return(output)
+	
 			
-		if not str1[0] in dict_choice:
+		if not str1[0] in move_dict or action_dict:
 			output = 'Fudge you'
 			if str1[0] == 'h':
 				menu = print_menu()
@@ -65,6 +111,16 @@ class Game:
 		if other._choice == self._winning_action:
 			return True
 		return False
+
+	def check_move(self, other):
+		str1 = other._choice.split(' ')		
+		
+		if not str1[0] in move_dict:
+			print('here1')
+			self._action = 'action'
+		else:
+			print('here2')
+			self._action = 'move'
 		
 
 class Player(Game):
@@ -81,7 +137,7 @@ class Player(Game):
 			'a giant rhinocerous with 12 horns!!', 
 			'a falling stone you pleb!', 
 			'tripping and falling on a rake that then flew up and hit a '
-			'match which hit lit some dynamite which exploded',
+			'match which lit some dynamite which exploded',
 			'seeing a squirrel and having a heart attack'
 			]
 			
@@ -93,8 +149,17 @@ class Scene(Game):
 	def __init__(self, winning_action, description):
 		super().__init__(winning_action)
 		self._description = description
+		self._item1 = None
+		self._item2 = None
 		
-	
+	def create_treasure(self, step):
+		if step == 0:
+			self._item1 = 'Helmet'
+			self._item2 = 'Prism'
+		elif step == 3:
+			self._item1 = 'Edelweiss'
+		
+		
 		
 
 			
