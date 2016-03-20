e5350c332fd23c1997aa8501623b39c87f9c4bf6
Derfador1 <Derfador1@gmail.com>
2016-03-19 20:35:38 -0400

    moved lists back into main and added comments

--- journey.py
+++ journey.py
@@ -22,26 +22,7 @@ def print_menu():
 		]
 	for item in menu:
 		print(item)
-		
-
-winning_options = [
-	'get all', 'open door',
-	'east', 'get edelweiss',
-	'up', 'enter cave', 
-	'light fire', 'wait', 
-	'put edelweiss in fire', 
-	'put helmet in statue', 
-	'put prism in pickle', 
-	'exit cave', 'north', 
-	'get meaning of life'
-	]
-
-scene_descrip = [
-	'You have entered a cave!',
-	'You are in a field',
-	'You entered an archway and now you are standing in poo',
-	'you entered a door, I am looking at you....but you dont see me'
-	]
+	
 
 def main():
 	print('Welcome to the interactive fiction game')
@@ -50,7 +31,27 @@ def main():
 	print('Now you might be wondering , who the hell is talking to me right now!')
 	print('Why the hell am I playing this game, I was just sipping coffee! Well')
 	print('no longer, you are now playing this game and choose rightly or you')
-	print('will die! (one hint, "look" to see what is around)')
+	print('will die! (hint, "look" to see what is around)')
+	print('(and "h" to open a menu of possible choices)')
+	
+	winning_options = [
+		'get all', 'open door',
+		'east', 'get edelweiss',
+		'up', 'enter cave', 
+		'light fire', 'wait', 
+		'put edelweiss in fire', 
+		'put helmet in statue', 
+		'put prism in pickle', 
+		'exit cave', 'north', 
+		'get meaning of life'
+		]
+
+	scene_descrip = [
+		'You have entered a cave!',
+		'You are in a field',
+		'You entered an archway and now you are standing in poo',
+		'you entered a door, I am looking at you....but you dont see me'
+		]
 	
 	step = 0
 	
@@ -60,11 +61,12 @@ def main():
 	scene = a.Scene(winning_options[step], scene_descrip[random.randint(0, 3)])
 		
 	while True:
+		#creates the treasure for the room
 		scene.create_treasure(step)
 		
 		player.choose()
 		os.system('clear')
-								
+		#checks if the choice was move choice or action choice						
 		scene.check_move(player)
 		
 		response = player.get_actions()
@@ -80,7 +82,7 @@ def main():
 			continue
 		elif response == 'looking...':
 			print(response + '\n' + scene._description)
-			
+			#prints what objects are on the floor before you based on 2 items, 1 item, or no items
 			if not scene._item1 == None and not scene._item2 == None:
 				print('Before you is an', scene._item1)
 				print('There is also a', scene._item2)
@@ -101,6 +103,7 @@ def main():
 		print(response)
 					
 		if response == 'getting...':
+			#prints what was got depending on 2 items , 1 item, or no items
 			player._lootbag.append(scene._item1)
 			player._lootbag.append(scene._item2)
 			
@@ -115,19 +118,22 @@ def main():
 		if scene.get_winning_actions(player):
 			print('That was the only correct option!')
 			answer.append(player._choice)
+			#increase step except for the last one
 			if step < 13:
 				step += 1
 				
 			if scene._action == 'move':
 				scene = a.Scene(winning_options[step], scene_descrip[random.randint(0, 3)])
 			else:
+				#updates scene instead of creating a new one
 				scene.scene_update(winning_options[step])
 
 		else:		#could remove this to allow player to keep going to get to good path the round about way
 			death = player.player_death()
 			print(death)
 			break
-			
+		
+		#mathces if answer and winning options are the same
 		if set(answer) == set(winning_options):
 			print('you have won')
 			break
@@ -136,6 +142,7 @@ def main():
 if __name__ == "__main__":
 	try:
 		main()
+	#catches the keyboard interrupt if ^C is used to end program
 	except KeyboardInterrupt:
 		print('\nInterrupted...')
 		try:
